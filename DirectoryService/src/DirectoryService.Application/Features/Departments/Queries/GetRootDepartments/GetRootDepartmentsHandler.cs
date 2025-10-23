b1c57bc4d3f7ae104965a3cf28e6a3658c73a9aa
etetudyyd <dima.ryzhkov@gmail.com>
2025-10-23 15:17:51 +0300

    [dev]: bug fix GetRootDepartments

--- DirectoryService/src/DirectoryService.Application/Features/Departments/Queries/GetRootDepartments/GetRootDepartmentsHandler.cs
+++ DirectoryService/src/DirectoryService.Application/Features/Departments/Queries/GetRootDepartments/GetRootDepartmentsHandler.cs
@@ -37,81 +37,6 @@ public class GetRootDepartmentsHandler : IQueryHandler<GetRootDepartmentsRespons
 
         using var connection = await _connectionFactory.CreateConnectionAsync(cancellationToken);
 
-        const string sql = @"WITH roots AS (
-                                SELECT 
-                                    d.id, 
-                                    d.name,
-                                    d.identifier,
-                                    d.parent_id AS ""parentId"",
-                                    d.created_at AS ""createdAt""                                
-                                FROM departments d
-                                WHERE parent_id IS NULL
-                                ORDER BY d.created_at
-                                OFFSET @Offset LIMIT @Limit
-                            )
-                            (
-	                            SELECT 
-		                            id,
-		                            ""name"",
-		                            identifier,
-		                            ""parentId"",
-		                            ""createdAt"", 
-		                            EXISTS(
-			                            SELECT 1 
-			                            FROM departments d 
-			                            WHERE d.parent_id = roots.id 
-			                            OFFSET @Prefetch LIMIT 1
-		                            ) AS ""hasMoreChildren""
-	                            FROM roots
-                            )        
-                            UNION ALL
-                            (
-                            SELECT 
-	                            c.id,
-	                            c.""name"",
-	                            c.identifier,
-	                            c.parent_id,
-	                            c.created_at,
-	                            FALSE ""hasMoreChildren""
-                            FROM roots
-                            CROSS JOIN LATERAL (
-                                SELECT
-    	                            d.id,
-                                    d.name,
-                                    d.identifier,
-                                    d.parent_id,
-                                    d.created_at
-                                FROM departments d
-                                WHERE d.parent_id = roots.id
-                                LIMIT @Prefetch
-                                ) AS c
-                            );";
-
-        var dynamicParameters = new DynamicParameters();
-
-        dynamicParameters.Add("Prefetch", query.DepartmentsRequest.Prefetch);
-        dynamicParameters.Add("Offset", (query.DepartmentsRequest.Page - 1) * query.DepartmentsRequest.PageSize);
-        dynamicParameters.Add("Limit", query.DepartmentsRequest.PageSize);
-
-        var result = await connection
-            .QueryAsync<DepartmentPrefetchResponse>(
-                sql,
-                param: dynamicParameters);
-
-        _logger.LogInformation("GetRootDepartmentsWithChilds was successfully executed.");
-        return new GetRootDepartmentsResponse(result.ToList());
-    }
-
-    /*public async Task<Result<GetRootDepartmentsResponse, Errors>> Handle(
-        GetRootDepartmentsQuery query,
-        CancellationToken cancellationToken)
-    {
-        var validationResult = await _validator.ValidateAsync(query, cancellationToken);
-        if (!validationResult.IsValid)
-            return validationResult.ToErrors();
-
-        using var connection = await _connectionFactory.CreateConnectionAsync(cancellationToken);
-
         var parameters = new DynamicParameters();
 
         parameters.Add("offset", (query.DepartmentsRequest.Page - 1) * query.DepartmentsRequest.PageSize, DbType.Int32);
@@ -126,45 +51,43 @@ public class GetRootDepartmentsHandler : IQueryHandler<GetRootDepartmentsRespons
                            d.name,
                            d.identifier,
                            d.path,
-                           d.parent_id,
-                           d.is_active,
-                           d.created_at,
-                           d.update_at
+                           d.parent_id AS ParentId,
+                           d.is_active AS IsActive,
+                           d.created_at AS CreatedAt,
+                           d.update_at AS UpdateAt
                     FROM departments d
                     WHERE d.parent_id IS NULL
                     ORDER BY d.created_at
                     OFFSET @offset LIMIT @root_limit
                 )
                 SELECT
-                    r.id,
-                    r.name,
-                    r.identifier,
-                    r.path,
-                    r.parent_id,
-                    r.is_active,
-                    r.created_at,
-                    r.update_at,
+                    r.id AS Id,
+                    r.name AS Name,
+                    r.identifier AS Identifier,
+                    r.path AS Path,
+                    r.ParentId,
+                    r.IsActive,
+                    r.CreatedAt,
+                    r.UpdateAt,
                     (EXISTS(
                         SELECT 1 FROM departments
                         WHERE parent_id = r.id
                         OFFSET @child_limit LIMIT 1
-                    ))::bool AS has_more_children
+                    ))::bool AS HasMoreChildren
                 FROM roots r
-
                 UNION ALL
-
                 SELECT
-                    c.id,
-                    c.name,
-                    c.identifier,
-                    c.path,
-                    c.parent_id,
-                    c.is_active,
-                    c.created_at,
-                    c.update_at,
+                    c.id AS Id,
+                    c.name AS Name,
+                    c.identifier AS Identifier,
+                    c.path AS Path,
+                    c.parent_id AS ParentId,
+                    c.is_active AS IsActive,
+                    c.created_at AS CreatedAt,
+                    c.update_at AS UpdateAt,
                     (EXISTS(
                         SELECT 1 FROM departments WHERE parent_id = c.id
-                    ))::bool AS has_more_children
+                    ))::bool AS HasMoreChildren
                 FROM roots r
                 CROSS JOIN LATERAL (
                     SELECT
@@ -188,5 +111,5 @@ public class GetRootDepartmentsHandler : IQueryHandler<GetRootDepartmentsRespons
         _logger.LogInformation("Departments was successfully founded!");
 
         return new GetRootDepartmentsResponse(departments.ToList());
-    }*/
+    }
 }
--- DirectoryService/src/DirectoryService.Contracts/Departments/Responses/DepartmentPrefetchResponse.cs
+++ DirectoryService/src/DirectoryService.Contracts/Departments/Responses/DepartmentPrefetchResponse.cs
@@ -2,7 +2,7 @@
 
 public sealed record DepartmentPrefetchResponse
 {
-    /*public Guid Id { get; init; }
+    public Guid Id { get; init; }
 
     public string Name { get; init; } = string.Empty;
 
@@ -18,51 +18,17 @@ public sealed record DepartmentPrefetchResponse
 
     public DateTime UpdateAt { get; init; }
 
+    public bool HasMoreChildren { get; init; }
+
     public List<DepartmentPrefetchResponse> Children { get; init; } = [];
 
-    public bool HasMoreChildren { get; init; }*/
-
-
-        public DepartmentPrefetchResponse(
-            Guid id,
-            string name,
-            string identifier,
-            DateTime createdAt,
-            bool hasMoreChildren,
-            List<ChildDepartmentDto> childs)
-        {
-            Id = id;
-            Name = name;
-            Identifier = identifier;
-            CreatedAt = createdAt;
-            HasMoreChildren = hasMoreChildren;
-            Childs = childs;
-        }
-
-        public Guid Id { get; init; }
-        public string Name { get; init; }
-        public string Identifier { get; init; }
-        public DateTime CreatedAt { get; init; }
-        public bool HasMoreChildren { get; init; }
-        public List<ChildDepartmentDto> Childs { get; init; } = [];
-    }
-
-public sealed record ChildDepartmentDto
-{
-    public ChildDepartmentDto(
-        Guid id,
-        string name,
-        string identifier,
-        DateTime createdAt)
-    {
-        Id = id;
-        Name = name;
-        Identifier = identifier;
-        CreatedAt = createdAt;
-    }
-
-    public Guid Id { get; init; }
-    public string Name { get; init; }
-    public string Identifier { get; init; }
-    public DateTime CreatedAt { get; init; }
+    /*c.id,
+    c.name,
+    c.identifier,
+    c.path,
+    c.parent_id,
+    c.is_active,
+    c.created_at,
+    c.update_at,
+    c.has_more_children*/
 }
